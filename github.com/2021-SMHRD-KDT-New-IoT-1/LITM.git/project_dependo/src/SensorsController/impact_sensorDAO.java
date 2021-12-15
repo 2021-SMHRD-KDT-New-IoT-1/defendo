4f966dcfcef280a445c059079ed75eda2310bb01
지웅 <yred0416@gmail.com>
2021-12-15 16:47:31 +0900

    회원가입 jdbc db경로 수정

--- github.com/2021-SMHRD-KDT-New-IoT-1/LITM.git/project_dependo/src/SensorsController/impact_sensorDAO.java
+++ github.com/2021-SMHRD-KDT-New-IoT-1/LITM.git/project_dependo/src/SensorsController/impact_sensorDAO.java
@@ -19,8 +19,8 @@ public class impact_sensorDAO {
 
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 			String db_url = "jdbc:oracle:thin:@project-db-stu.ddns.net:1524:xe";
-			String db_id = "campus_a_5_1025";
-			String db_pw = "smhrd5";
+			String db_id = "campus_a_3_1215";
+			String db_pw = "smhrd3";
 			conn = DriverManager.getConnection(db_url, db_id, db_pw);
 
 		} catch (ClassNotFoundException e) {
