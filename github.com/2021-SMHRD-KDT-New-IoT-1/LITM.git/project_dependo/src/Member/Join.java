01ed3c28d74fe5e96692331418cb980faaa24542
smhrd <smhrd@61.80.80.135>
2021-12-16 12:00:04 +0900

    12시_혜지

---
+++ github.com/2021-SMHRD-KDT-New-IoT-1/LITM.git/project_dependo/src/Member/Join.java
@@ -0,0 +1,51 @@
+package Member;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/Join")
+public class Join extends HttpServlet {
+   private static final long serialVersionUID = 1L;
+
+
+   protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+request.setCharacterEncoding("euc-kr");
+      
+
+      String id = request.getParameter("id");
+      String password = request.getParameter("password");
+      
+      String name = request.getParameter("name");
+      String tel = request.getParameter("tel");
+      String dept = request.getParameter("dept");
+      String check_manager = request.getParameter("check_manager");
+      
+      System.out.println(id+"/"+password+"/"+tel+"/"+dept+"/"+check_manager);
+
+      
+      MemberDAO dao= new MemberDAO();
+      
+      int cnt = dao.Join(id, password, name, tel, dept);
+      
+      System.out.println(cnt);
+      
+      if(cnt > 0) {
+         System.out.println("회원가입성공");
+         response.sendRedirect("http://localhost:8085/project_dependo/defendo/index_before.jsp");
+      }else {
+         System.out.println("회원가입실패");
+         response.sendRedirect("http://localhost:8085/project_dependo/defendo/index_before.jsp");
+      }
+      
+      
+      
+      
+   }
+
+}
